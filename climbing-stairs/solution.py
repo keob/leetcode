297db03f7785243980f7c6d21e07dce88c66d34f
keob <bugbinbo@outlook.com>
2019-12-20 15:05:09 +0800

    fix climbing stairs

--- climbing-stairs/solution.py
+++ climbing-stairs/solution.py
@@ -7,6 +7,6 @@ class Solution:
         f[1] = 1
         f[2] = 2
         for i in range(3, n + 1):
-            f[1] = f[i - 1] + f[i - 2]
+            f[i] = f[i - 1] + f[i - 2]
 
         return f[n]
