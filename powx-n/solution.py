d56f4d1a4c6125ea8cfa28a34bcf8abbb89301af
keob <bugbinbo@outlook.com>
2019-12-20 15:13:41 +0800

    Add powx n solution

---
+++ powx-n/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        if n < 0:
+            x = 1 / x
+            n = -n
+        rest = 1
+        while n >= 1:
+            if n % 2 == 1:
+                rest *= x
+            x *= x
+            n //= 2
+
+        return rest
