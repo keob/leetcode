8829bf89db4ed79fbc7e56eb767e34fb04c4efd2
keob <bugbinbo@outlook.com>
2019-12-20 14:30:26 +0800

    Add length of longest substring

---
+++ longest-substring-without-repeating-character/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longStr = set([])
+        curStr = set([])
+        max = 0
+        for i in range(len(s)):
+            longStr = set([s[i]])
+            curStr = set([s[i]])
+            for j in range(i + 1, len(s)):
+                longStr.add(s[j])
+                if len(longStr) > len(curStr):
+                    curStr.add(s[j])
+                else:
+                    break
+            if len(longStr) > max:
+                max = len(longStr)
+        return max
