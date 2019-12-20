9f90c353ec1888c27d3280683a69fe9bec84ba26
keob <bugbinbo@outlook.com>
2019-12-20 14:45:51 +0800

    Add plus one solution

---
+++ plus-one/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        return [
+            int(x) for x in str(int(''.join([str(i) for i in digits])) + 1)
+        ]
