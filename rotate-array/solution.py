68ed8010d702b2d952b3ee95546628dce8a19830
keob <bugbinbo@outlook.com>
2019-12-20 14:23:07 +0800

    Add rotate array solution

---
+++ rotate-array/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def rotateArray(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+        nums[:] = nums[n - k:] + nums[:n - k]
