a0fd4082a7daf263b25721175ea651c1698dc45c
keob <bugbinbo@outlook.com>
2019-12-20 14:10:04 +0800

    Add two sum solution

---
+++ twosum/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution(object):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        num = len(nums)
+        for x in range(0, num):
+            if nums[x] in dic:
+                return [dic[nums[x]], x]
+            else:
+                dic[target - nums[x]] = x
