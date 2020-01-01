f723ac5a89bd2f90bbed5bb2263341c4ea704018
keob <bugbinbo@outlook.com>
2020-01-01 20:15:22 +0800

    Add longest increasing subsequence solution

---
+++ longest-increasing-subsequence/solution.py
@@ -0,0 +1,10 @@
+from typing import List
+import bisect
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        minend = [float("inf")] * len(nums)
+        for n in nums:
+            minend[bisect.bisect_left(minend, n)] = n
+        return bisect.bisect_left(minend, float("inf"))
