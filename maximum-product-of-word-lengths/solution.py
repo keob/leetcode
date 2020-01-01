0afbe40d1df20424014d0ff5d009c217aa4ec0a3
keob <bugbinbo@outlook.com>
2020-01-01 20:30:49 +0800

    Add maximum product of word lengths solution

---
+++ maximum-product-of-word-lengths/solution.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        d = {}
+        for word in words:
+            mask = 0
+            for w in set(word):
+                mask |= (1 << (ord(w) - ord('a')))
+            d[mask] = max(d.get(mask, 0), len(word))
+        return max([d[x] * d[y] for x in d for y in d if not x & y] or [0])
