1f523a23ac3f4849b8ac44ce0c051ba5d69f7e39
keob <bugbinbo@outlook.com>
2020-01-03 08:43:36 +0800

    Add happy number solution

---
+++ happy-number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        while True:
+            n = sum([int(i)**2 for i in str(n)])
+            if n == 4:
+                return False
+            if n == 1:
+                return True
