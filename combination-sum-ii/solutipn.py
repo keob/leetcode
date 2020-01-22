61d72b3b2d82712cb24141206da1b7bd6b85dc2d
keob <bugbinbo@outlook.com>
2020-01-22 15:51:35 +0800

    format code

--- combination-sum-ii/solutipn.py
+++ combination-sum-ii/solutipn.py
@@ -2,7 +2,8 @@ from typing import List
 
 
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, candidates: List[int],
+                        target: int) -> List[List[int]]:
         def dfs(begin, path, residue):
             if residue == 0:
                 res.append(path[:])
@@ -12,7 +13,8 @@ class Solution:
                 if candidates[index] > residue:
                     break
 
-                if index > begin and candidates[index - 1] == candidates[index]:
+                if index > begin and candidates[index -
+                                                1] == candidates[index]:
                     continue
 
                 path.append(candidates[index])
--- combination-sum/solution.py
+++ combination-sum/solution.py
@@ -2,7 +2,8 @@ from typing import List
 
 
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum(self, candidates: List[int],
+                       target: int) -> List[List[int]]:
         if len(candidates) == 0:
             return []
 
@@ -12,7 +13,7 @@ class Solution:
             if summ == target:
                 ans.append(path)
             elif summ > target:
-                return 
+                return
             elif summ < target:
                 for i in range(index, len(candidates)):
                     backtrack(path + [candidates[i]], summ + candidates[i], i)
