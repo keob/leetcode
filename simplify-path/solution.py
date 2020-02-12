dd597e729fabddea3b6f30b25682b2e28a684d58
keob <bugbinbo@outlook.com>
2020-02-12 20:43:34 +0800

    Add simplify path

---
+++ simplify-path/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        path = path.split("/")
+
+        for item in path:
+            if item == "..":
+                if stack:
+                    stack.pop()
+            elif item and item != ".":
+                stack.append(item)
+
+        return "/" + "/".join(stack)
