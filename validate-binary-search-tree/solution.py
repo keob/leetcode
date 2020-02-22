c469df01f93a216197c19005db494805e0b429fa
keob <bugbinbo@outlook.com>
2020-02-22 15:17:38 +0800

    Add validate binary search tree

---
+++ validate-binary-search-tree/solution.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        inorder = self.inorder(root)
+        return inorder == list(sorted(set(inorder)))
+
+    def inorder(self, root):
+        if root is None:
+            return []
+        return self.inorder(root.left) + [root.val] + self.inorder(root.right)
