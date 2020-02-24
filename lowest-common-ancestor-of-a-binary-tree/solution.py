95563b6ad33ee71ff1988c0b942475e41cf1d8a3
keob <bugbinbo@outlook.com>
2020-02-24 15:31:01 +0800

    Update

--- lowest-common-ancestor-of-a-binary-tree/solution.py
+++ lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -1,29 +1,18 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
 class Solution:
-
-    def __init__(self):
-        self.ans = None
-
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        def recurse_tree(current_node):
-            if not current_node:
-                return False
-
-            left = recurse_tree(current_node.left)
-
-            right = recurse_tree(current_node.right)
-
-            mid = current_node == p or current_node == q
-
-            if mid + left + right >= 2:
-                self.ans = current_node
-
-            return mid or left or right
-
-        recurse_tree(root)
-        return self.ans
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
