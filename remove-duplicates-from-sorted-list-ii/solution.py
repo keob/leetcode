f692dd360b8497ba598170278931b0c00d61a2a1
keob <bugbinbo@outlook.com>
2020-02-25 20:30:13 +0800

    Add remove duplicates from sorted list ii solution

---
+++ remove-duplicates-from-sorted-list-ii/solution.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        if head.next and head.val == head.next.val:
+            while head.next != None and head.val == head.next.val:
+                head = head.next
+            return self.deleteDuplicates(head.next)
+        else:
+            head.next = self.deleteDuplicates(head.next)
+        return head
