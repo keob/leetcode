38b02f54e62a3b20a935e7129efe7d636dd05442
kebo <bugbinbo@outlook.com>
2020-03-12 16:55:52 +0800

    Add moving stones until consecutive solution

--- moving-stones-until-consecutive/solution.py
+++ moving-stones-until-consecutive/solution.py
@@ -3,6 +3,6 @@ from typing import List
 
 class Solution:
     def numMovesStones(self, a: int, b: int, c: int) -> List[int]:
-        data=sorted([a, b, c])
-        l, r= data[1] - data[0] - 1, data[2] - data[1] -1
+        data = sorted([a, b, c])
+        l, r = data[1] - data[0] - 1, data[2] - data[1] -1
         return [1 if l == 1 or r == 1 else bool(l) + bool(r), l + r]
