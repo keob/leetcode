3243e18859d58557ad2133d01b06db55cc8c77c1
kebo <bugbinbo@outlook.com>
2020-03-26 18:25:51 +0800

    Add available captures for rook solution

---
+++ available-captures-for-rook/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def numRookCaptures(self, board: List[List[str]]) -> int:
+        res, st, ed = 0, 0, 0
+        dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+        for i in range(8):
+            for j in range(8):
+                if board[i][j] == "R":
+                    st, ed = i, j
+        for i in range(4):
+            step = 0
+            while True:
+                tx = st + step * dx[i]
+                ty = ed + step * dy[i]
+                if tx < 0 or tx >= 8 or ty < 0 or ty >= 8 or board[tx][ty] == "B":
+                    break
+                if board[tx][ty] == "p":
+                    res += 1
+                    break
+                step += 1
+        return res
