60615473979fab2d9d1defa7ce2e927814713b5a
kebo <bugbinbo@outlook.com>
2020-04-10 20:00:21 +0800

    Add restore ip addresses solution

---
+++ restore-ip-addresses/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        n = len(s)
+
+        def backtrack(i, tmp, flag):
+            if i == n and flag == 0:
+                res.append(tmp[:-1])
+                return
+            if flag < 0:
+                return
+            for j in range(i, i + 3):
+                if j < n:
+                    if i == j and s[j] == "0":
+                        backtrack(j + 1, tmp + s[j] + ".", flag - 1)
+                        break
+                    if 0 < int(s[i:j + 1]) <= 255:
+                        backtrack(j + 1, tmp + s[i:j + 1] + ".", flag - 1)
+
+        backtrack(0, "", 4)
+        return res
