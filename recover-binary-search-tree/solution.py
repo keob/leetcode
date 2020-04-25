8235306abdbd0f4a936c67cca6d662fba515fa39
kebo <bugbinbo@outlook.com>
2020-04-25 20:08:27 +0800

    Add recover binary search tree solution

---
+++ recover-binary-search-tree/solution.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def recoverTree(self, root: TreeNode) -> None:
+
+        def inorder(r: TreeNode) -> List[int]:
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+        
+        def find_two_swapped(nums: List[int]) -> (int, int):
+            n = len(nums)
+            x = y = -1
+            for i in range(n - 1):
+                if nums[i + 1] < nums[i]:
+                    y = nums[i + 1]
+                    if x == -1:     
+                        x = nums[i]
+                    else:           
+                        break
+            return x, y
+        
+        def recover(r: TreeNode, count: int):
+            if r:
+                if r.val == x or r.val == y:
+                    r.val = y if r.val == x else x
+                    count -= 1
+                    if count == 0:
+                        return      
+                recover(r.left, count)
+                recover(r.right, count)
+            
+        nums = inorder(root)
+        x, y = find_two_swapped(nums)
+        recover(root, 2)
