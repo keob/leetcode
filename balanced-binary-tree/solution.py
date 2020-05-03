f79719d3ef7e2fb848abfc0af6239bb9b81de8bb
kebo <bugbinbo@outlook.com>
2020-05-03 20:39:52 +0800

    Add balanced binary tree solution

---
+++ balanced-binary-tree/solution.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        self.res = True
+
+        def helper(root):
+            if not root:
+                return 0
+            left = helper(root.left) + 1
+            right = helper(root.right) + 1
+            if abs(right - left) > 1: 
+                self.res = False
+            return max(left, right)
+
+        helper(root)
+
+        return self.res
