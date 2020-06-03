75115f3d9ac3fa60106fef51c51334b06bd661d2
kebo <bugbinbo@outlook.com>
2020-06-03 20:19:02 +0800

    Add new 21 game solution

---
+++ new-21-game/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def new21Game(self, N: int, K: int, W: int) -> float:
+        dp = [None] * (K + W)
+        s = 0
+
+        for i in range(K, K + W):
+            dp[i] = 1 if i <= N else 0
+            s += dp[i]
+        for i in range(K - 1, -1, -1):
+            dp[i] = s / W
+            s = s - dp[i+W] + dp[i]
+
+        return dp[0]
