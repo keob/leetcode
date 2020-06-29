159b51285878f1281e7f9fe1773fba161f11f3cf
kebo <isgopher@outlook.com>
2020-06-29 16:19:46 +0800

    Add kth largest element in an array solution

---
+++ kth-largest-element-in-an-array/solution.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def partition(left, right):
+            pivot = nums[left]
+            l = left + 1
+            r = right
+
+            while l <= r:
+                if nums[l] < pivot and nums[r] > pivot:
+                    nums[l], nums[r] = nums[r], nums[l]
+                if nums[l] >= pivot:
+                    l += 1
+                if nums[r] <= pivot:
+                    r -= 1
+            nums[r], nums[left] = nums[left], nums[r]
+            return r
+
+        left = 0
+        right = len(nums) - 1
+
+        while 1:
+            idx = partition(left, right)
+            if idx == k - 1:
+                return nums[idx]
+            if idx < k - 1:
+                left = idx + 1
+            if idx > k - 1:
+                right = idx - 1
