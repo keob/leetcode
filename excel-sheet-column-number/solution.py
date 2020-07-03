b3328bd1b9245c6e85c00667a35d00c7ccb55472
kebo <isgopher@outlook.com>
2020-07-03 19:54:26 +0800

    Add excel sheet column number solution

---
+++ excel-sheet-column-number/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        res = 0
+        bit = 1
+
+        for a in s[::-1]:
+            res += (ord(a) - 64) * bit
+            bit *= 26
+        return res
