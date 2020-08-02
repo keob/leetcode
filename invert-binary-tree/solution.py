3f79abbe457cb4314c5a0ddc47f3a34320d3f8e2
kebo <isgopher@outlook.com>
2020-08-02 18:30:12 +0800

    Add invert binary tree solution

---
+++ invert-binary-tree/solution.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root == None:
+            return None
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
