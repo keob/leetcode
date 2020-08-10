2dd031220851c26578838bc557cbac35cecdb6c4
kebo <isgopher@outlook.com>
2020-08-10 17:48:40 +0800

    Add count binary substrings solution

---
+++ count-binary-substrings/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        seq = [0, 1]
+        res = []
+
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                seq[1] += 1
+            else:
+                res.append(min(seq))
+                seq[0] = seq[1]
+                seq[1] = 1
+
+        res.append(min(seq))
+
+        return sum(res)
