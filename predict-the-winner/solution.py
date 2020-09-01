00be7924723b0c6034248810c00b8b6f09831d1b
kebo <bugbinbo@outlook.com>
2020-09-01 21:14:05 +0800

    Add predict the winner solution

---
+++ predict-the-winner/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        length = len(nums)
+        dp = [0] * length
+
+        for i, num in enumerate(nums):
+            dp[i] = num
+        for i in range(length - 2, -1, -1):
+            for j in range(i + 1, length):
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1])
+
+        return dp[length - 1] >= 0
