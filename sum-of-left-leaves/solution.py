32e704ff6bcfb78c29b9dcb0ac33d62dbea968aa
kebo <isgopher@outlook.com>
2020-09-19 17:45:01 +0800

    Add sum of left leaves solution

---
+++ sum-of-left-leaves/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        isLeafNode = lambda node: not node.left and not node.right
+
+        def dfs(node: TreeNode) -> int:
+            ans = 0
+            if node.left:
+                ans += node.left.val if isLeafNode(node.left) else dfs(node.left)
+            if node.right and not isLeafNode(node.right):
+                ans += dfs(node.right)
+            return ans
+
+        return dfs(root) if root else 0
